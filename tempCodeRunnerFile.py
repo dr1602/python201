08e92e6c47939446293a737582e395870a0b6f70
dr1602 <david.arangelg@gmail.com>
2024-09-01 19:52:08 -0600

    retorna pruebas

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,36 +1,11 @@
-matches = [
-  {
-    'home_team': 'Bolivia',
-    'away_team': 'Uruguay',
-    'home_team_score': 3,
-    'away_team_score': 1,
-    'home_team_result': 'Win'
-  },
-  {
-    'home_team': 'Brazil',
-    'away_team': 'Mexico',
-    'home_team_score': 1,
-    'away_team_score': 1,
-    'home_team_result': 'Draw'
-  },
-  {
-    'home_team': 'Ecuador',
-    'away_team': 'Venezuela',
-    'home_team_score': 5,
-    'away_team_score': 0,
-    'home_team_result': 'Win'
-  },
-]
 
-# filtrar lista con solo los equipos que hayan ganado
+def filter_by_length(words):
+   # Escribe tu solución 👇
+   
+   new_list = list(filter(lambda item: len(item) >= 4, words))
+   
+   return new_list
 
-print(matches)
-print(len(matches))
-
-new_list = list(filter(lambda item: item['home_team_result'] == 'Win', matches))
-print(new_list)
-print(len(new_list))
-
-# con filter no modificamos el estado de la lista original
-print(matches)
-print(len(matches))
+words = ['amor', 'sol', 'piedra', 'día']
+response = filter_by_length(words)
+print(response)
